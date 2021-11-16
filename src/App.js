2b599ff15701b731878337314057f026a5efab20
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-11-16 17:12:04 +0100

    react ewa app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react'
-import Laptop from './views/Laptop'
+//import Laptop from './views/Laptop'
 import "./App.css"
+import Register_Page from './views/Register_Page';
+import LoginPage from './views/LoginPage'
+
 
 function App() {
+  
   return (
     <div className="container">
-      <Laptop name= "Apple" color="gold" madeIn="Nigeria" buttonColor= {"red"}/>
-      <Laptop name= "Dell" color="grey" madeIn="Lagos" buttonColor={"blue"}/>
+      <Register_Page/>
+      {/* <LoginPage/> */}
+    
+      {/* <Laptop name= "Apple" color="gold" madeIn="Nigeria" buttonColor= {"red"}/>
+      <Laptop name= "Dell" color="grey" madeIn="Lagos" buttonColor={"blue"}/> */}
     </div>
   )
 }
