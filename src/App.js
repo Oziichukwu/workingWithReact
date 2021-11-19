255142111aa6647347ab79a177d00a0f6fc7ffc9
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-11-19 11:25:51 +0100

    ewa app updated

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import React from 'react'
-//import Laptop from './views/Laptop'
 import "./App.css"
-import Register_Page from './views/Register_Page';
-import LoginPage from './views/LoginPage'
+import Auth from './ewa/views/auth/Auth'
+
 
 
 function App() {
   
   return (
     <div className="container">
-      <Register_Page/>
-      {/* <LoginPage/> */}
-    
-      {/* <Laptop name= "Apple" color="gold" madeIn="Nigeria" buttonColor= {"red"}/>
-      <Laptop name= "Dell" color="grey" madeIn="Lagos" buttonColor={"blue"}/> */}
+
+      <Auth/>
+      
     </div>
   )
 }
