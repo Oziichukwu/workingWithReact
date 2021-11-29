145974eb0963fa5681ca9f8d61f8a29f08aa1b92
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-11-29 16:07:45 +0100

    ewa app

--- src/ewa/views/auth/Auth.js
+++ src/ewa/views/auth/Auth.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Login from '../../component/auth/login/Login'
+import Register from '../../component/auth/register/Register'
 
 const Auth = ()=> {
+    const [state, setState] = useState("login") 
     return (
         <div>
-            <Login/>
+            {state === "login" ? <Login setState={setState}/> : <Register setState={setState}/>}
+     
         </div>
     )
 }
