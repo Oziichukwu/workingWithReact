d6bd0e326582a9638ecd7fd0f84cfc5410a682db
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-11-29 16:08:20 +0100

    ewa app

--- src/ewa/component/auth/register/Register.js
+++ src/ewa/component/auth/register/Register.js
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Input from '../../reusables/Input'
+import './register.css'
+import img from "../../../../asserts/background_circle.svg"
+import image from "../../../../asserts/background_circle_bottom.svg"
+import Button from '../../reusables/Button'
 
-const Register=()=> {
+const Register=(props)=> {
+
+    let {setState} = props
 
     const inputField = [
         {label :"firstName",  placeholder :"Enter your FirstName"},
@@ -9,16 +16,29 @@ const Register=()=> {
         {label :"Password",  placeholder :"Enter your Password"},
 ]
     return (
-        <div className="Register_container">
+        <div className="register_container">
             <div className="register_header">
                 <div className="ewa_logo">
 
                 </div>
-                    <button>
-                        REGISTER
+                    <button onClick={()=>setState("login")}>
+                        Login
                     </button>
             </div>
-        </div>
+            <img className="top_circle" src={img} alt="circlebackground"/>
+            <div className="text_container">
+
+            <h3>Welcome to Ewa</h3>
+
+            <p>Where your cravings
+                 for beans are satisfied</p>
+            </div>
+                <div className="form_container">
+                {inputField.map((field,index)=><Input key={index} field={field}/>)}
+                </div>
+                <Button name="Register"/>
+                <img className="bottom_circle" src={image} alt="circlebackgroundbottom"/>
+            </div>
     )
 }
 
